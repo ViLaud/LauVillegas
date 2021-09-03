d4b345eab8f5fef138776eaf3d59f23dbeb37a04
Laura Villegas <vils.in.town@gmail.com>
2021-09-02 22:33:12 -0700

    Started Main Layout Components

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,19 +1,20 @@
 import Link from 'next/link'
 import styles from '../../styles/Home.module.css'
 import Head from 'next/head'
-
+import Layout from '../../components/layout'
 
 export default function blog () {
     return (
-        <div className={styles.container}>
-        <Head>
-          <title>Lau's Blog</title>
-          <link rel="icon" href="images/code.jpg" />
-        </Head>
-        <main className={styles.main}>
-        <h1>My Blog</h1>
-        <Link href="/"><a>Take me home</a></Link>
-        </main>
-        </div>
+        <Layout>
+            <Head>
+                <title>Lau's Blog</title>
+                <link rel="icon" href="images/code.jpg" />
+            </Head>
+
+            <main className={styles.main}>
+                <h1>Super Rad Blog</h1>
+                <Link href="/"><a>Take me home</a></Link>
+            </main>
+        </Layout>
     )
 }
